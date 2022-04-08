29c0633240d7ad13cf94c2912f3c644de136d81f
alekseyZhizhin <zhizhin309@yandex.ru>
2022-04-08 13:30:38 +0300

    update server

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -78,12 +78,6 @@ public class Server {
 //                                for (SocketChannel user:sockets.keySet()){
 //                                    user.write(buffer);
 //                                }
-//                                socketChannel.write(buffer);
-                                // По идее мы проходимся по пользователям, которые сейчас онлайн
-                                // и отправляем им сообщение, которое получили
-                                for (SocketChannel user:sockets.keySet()){
-                                    user.write(buffer);
-                                }
 
                                 log("Пользователь: [" +
                                         socketChannel.getRemoteAddress() + "] отправил сообщение: " + clientMsg);
